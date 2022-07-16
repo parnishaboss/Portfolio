8d70469e92f22237f858e2d092d6c7d94a6dd0a8
parnishaboss <zhora0@mail.ru>
2022-07-17 00:05:14 +0300

    1st step (added)

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './Skills.module.css';
 import sContainer from "../common/styles/Container.module.css";
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
     return (
@@ -8,7 +9,12 @@ export const Skills = () => {
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>My skills</h2>
                 <div className={s.skills}>
-
+                    <Skill title='js'
+                           discription='Privet 1'/>
+                    <Skill title='css'
+                           discription='Privet 2'/>
+                    <Skill title='react'
+                           discription='Privet 3'/>
                 </div>
             </div>
         </div>
---
+++ src/skills/skill/Skill.js
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './Skill.module.css';
+
+
+export const Skill = (props) => {
+    return (
+        <div className={s.skill}>
+            <div className={s.skillIcon}></div>
+            <h3>{props.title}</h3>
+            <span className={s.discription}>
+                {props.discription}
+            </span>
+        </div>
+    );
+}
+
