0ab98bcaa5b037c845c78f0eb3c11b40e4bb74ee
parnishaboss <zhora0@mail.ru>
2022-07-21 21:04:55 +0300

    2st step (added)

--- src/new/New.jsx
+++ src/new/New.jsx
@@ -6,8 +6,17 @@ export const New = () => {
     return (
         <div className={s.contactBlock}>
             <div className={sContainer.container}>
-                <h1 className={s.mainTitle}>Sasha Zhdanovich</h1>
-                <p className={s.mainDiscription}>Front-end Developer</p>
+                <div className={s.text}>
+                    <div className={s.firstWords}>
+                        <span className={s.firstWord}>Hello</span>
+                        <p className={s.otherWords}>I am</p>
+                    </div>
+                    <h1 className={s.mainTitle}>Sasha </h1>
+                    <h1 className={s.mainTitle}>Zhdanovich</h1>
+                    <p className={s.mainDiscription}>Front-end Developer</p>
+                </div>
+                <div className={s.mainFoto}>
+                </div>
             </div>
         </div>
     )
